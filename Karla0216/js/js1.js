54dfb1baeee80393c224b04ecde5cc9a14a96aaf
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-05 11:06:07 -0500

    avances

--- Karla0216/js/js1.js
+++ Karla0216/js/js1.js
@@ -7,6 +7,28 @@ console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
 console.log(`El tipo de dato de js ${typeof 'ja'}`);
 
 
+/*  Operadores de comparacion y logicos
+    == es igual
+    < menor que
+    > mayor que 
+    ! not
+   || or
+   && and */
 
-console.log(`El resultado de : ${3 > 1 || 3 == 2}`);
+console.log(`El resultado de : ${!3 > 1 || 3 == 2}`);
+
+
+/* Operador ternario 
+    ? */
+
+console.log( !3 > 2 ? "verdadero" : "falso");
+
+
+/* Declaracion de Variables y Constantes */
+
+/* hay que poner -let- */
+
+/* ------ */
+
+const UNI = "UANL";
 
