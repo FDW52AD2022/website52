231326fe30c951811e999be2b5ab86483f7e0ffc
AlexUnders <tonnigbsm@gmail.com>
2022-10-05 11:07:03 -0500

    Lo de hoy

---
+++ AlexUnders/js/js1.js
@@ -0,0 +1,21 @@
+
+console.log("Hola javascript");
+console.warn("hola a todos");
+console.error("saludos") /* objeto, después del objeto es un método*/
+/* Console  clase 
+CONSOLE  constante 
+_HHH
+___JJJ___ */
+
+console.log(`El tipo de dato de false ${typeof false}`);
+
+console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
+
+console.log(`El tipo de dato de js ${typeof 'js'}`);
+
+console.log(`El resultado de:
+${3>1||3==2}`);
+
+console.log(3>2 ? "verdadero" : "falso");
+
+const UNI = "UANL";
