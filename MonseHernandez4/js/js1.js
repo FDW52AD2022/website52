38b22742ae8c94280bd4257d23483429b693a8fa
MonseHernandez4 <monserrat.hernandezen@uanl.edu.mx>
2022-10-05 11:08:03 -0500

    Clase 5 Oct

--- MonseHernandez4/js/js1.js
+++ MonseHernandez4/js/js1.js
@@ -6,11 +6,12 @@ console.error("Saludos");
 //CONSOLE (constante)
 //Console (clase)
 
-//console.log('El tipo de dato de false ${typeof false}');
-
-//console.log('El resultado de: ${3 > 1 || 3 == 2}')
+console.log(`El tipo de dato de false ${typeof false}`);
+console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
+console.log(`El tipo de dato de js ${typeof 'js'}`);
 
 console.log( 3 > 2 ? "verdadero":"falso" );
 
 const UNI= "UANL";
 
+UNI = "patito"
