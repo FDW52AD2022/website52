c600ed9274b499a7ec8f705d7232af53bacf0f31
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-05 10:48:08 -0500

    avances

---
+++ Karla0216/js/js1.js
@@ -0,0 +1,12 @@
+console.log("Hola Javascript");
+console.warn("Hola a todos");
+console.error("Saludos");
+
+console.log(`El tipo de dato de false ${typeof false}`);
+console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
+console.log(`El tipo de dato de js ${typeof 'ja'}`);
+
+
+
+console.log(`El resultado de : ${3 > 1 || 3 == 2}`);
+
