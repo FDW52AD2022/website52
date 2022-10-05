96aae75d7d847fd37d4a3b4db3c6c05658419c2e
Angel2472 <angel.gzz373@gmail.com>
2022-10-05 11:17:12 -0500

    Se agrego archivos

---
+++ Angel2472/js/js1.js
@@ -0,0 +1,22 @@
+console.log("Hola Javascript ");
+console.warn("Hola a todos");
+console.error("Salu2");
+
+
+console.log(`El tipo de dato es falso, ${typeof false}`);
+console.log(`El tipo de dato de 5.5, es  ${typeof 5.5}`);
+console.log(`El tipo de dato de js, es ${typeof `js`}`);
+
+
+
+
+console.log("La linea marca si 3 es menor a 2 o verdadero o falso");
+console.log( 3>2 ? "Verdadero, true":"Falso, false"); 
+console.log( !3>2 ? "Verdadero, true":"Falso, false"); 
+
+console.log("El const es para fijar un valor para que nadie lo cambie, incluso otro programador, solo el que lo esta definiendo")
+const UNI="UANL";
+UNI= "PATITO"
+
+
+
