edafc9b6238c156ec466d7411ab4e0f83ab817c3
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-10 09:20:21 -0500

    Javascript 2

---
+++ DiegoMontalvo469/js/js2.js
@@ -0,0 +1,24 @@
+/* Ejemplo arreglo */
+
+let equipos = ["tigres","rayados","america"];
+
+console.log("Tipo de dato",
+            typeof equipos);
+
+console.log("Datos",
+            equipos);
+
+console.log("Total equipos",
+            equipos.length);
+
+console.log("Primer equipo",
+            equipos[0]);
+
+console.log("Ultimo equipo",
+            equipos[-1]);
+
+console.log("Último",
+            equipos[equipos.length-1]);
+
+
+
