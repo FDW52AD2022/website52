5cca36494f97e89646c78de5710410266d6d4bc0
RolandoRangelGarza <rolando.rangelgr@uanl.edu.mx>
2022-10-10 11:02:32 -0500

    espacio

--- RolandoRangelGarza/js/programa1ejemplo.js
+++ RolandoRangelGarza/js/programa1ejemplo.js
@@ -10,7 +10,7 @@ if( horas <= 40 ) {
     let salario_total = horas * pago_hora;
     console.log(salario_total);
 }
-/* Si tiene horas extras, a partir de 41 */
+/* Si tiene horas extras, a partir de 41 en adelante */
 else {
     let salario_total = (40 * pago_hora) + ((horas-40) * (pago_hora*3));
     console.log(salario_total);
