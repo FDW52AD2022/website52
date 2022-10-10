5b21412dd7a162a3359dc8856836dd414079decf
Iván Briones <ivan.brioneslzr@uanl.edu.mx>
2022-10-10 09:28:26 -0500

    Archivos clase 10-oct

---
+++ Ivan-BS/js/js2.js
@@ -0,0 +1,28 @@
+/* Ejemplo arreglo */
+
+let equipos = ["tigre" , "rayados" , "america"];
+
+console.log("Tipo de dato",
+            typeof equipos);
+        
+console.log("Datos",
+            equipos);
+
+console.log("Total equipos",
+            equipos.length);
+        
+console.log("Primer equipo",
+            equipos[0]);
+
+console.log("ultimo equipo",
+            equipos[equipos.length-1]);
+
+equipos.push("necaxa");
+
+console.log(equipos);
+console.log("ultimo", equipos.pop());
+
+/* Definir un objeto */
+let grupo52 = {
+    
+}
