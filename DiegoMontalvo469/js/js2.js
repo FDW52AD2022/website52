f3ab7c29d7704117188a0954842725fa31948546
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-10 09:41:04 -0500

    javascript2

--- DiegoMontalvo469/js/js2.js
+++ DiegoMontalvo469/js/js2.js
@@ -20,5 +20,31 @@ console.log("Ultimo equipo",
 console.log("Último",
             equipos[equipos.length-1]);
 
+equipos.push("necaxa");
+
+console.log(equipos);
+console.log("ultimo", equipos.pop());
+
+
+/* Definir un objeto  */
+let materias = ["web","conta","costos"];
+
+let grupo52 = {
+     "nombre" : "grupo 52",
+     "semestre" : 5,
+     "carrera" : "LTI",
+     "materias" : materias,
+};
+
+console.log(grupo52);
+
+console.log("nombre:", grupo52[0]) /* error */
+console.log("nombre:", grupo52["nombre"]) /* sí :D */
+console.log("nombre:", grupo52.nombre);
+console.log("Primer materia:",
+             grupo52["materias"][0] );
+
+console.log("Primer materia:",
+             grupo52.materias[0] );
 
 
