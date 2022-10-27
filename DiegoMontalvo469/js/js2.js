42c414a24d64ff504b9d835119b93dc663cac0c8
DiegoMontalvo469 <dmontalvo469@gmail.com>
2022-10-27 18:12:48 -0500

    js2

--- DiegoMontalvo469/js/js2.js
+++ DiegoMontalvo469/js/js2.js
@@ -3,21 +3,21 @@
 let equipos = ["tigres","rayados","america"];
 
 console.log("Tipo de dato",
-            typeof equipos);
+            typeof equipos );
 
 console.log("Datos",
             equipos);
 
-console.log("Total equipos",
+console.log("total equipos",
             equipos.length);
 
 console.log("Primer equipo",
             equipos[0]);
 
-console.log("Ultimo equipo",
+console.log("ultimo equipo",
             equipos[-1]);
 
-console.log("Último",
+console.log("ultimo equipo",
             equipos[equipos.length-1]);
 
 equipos.push("necaxa");
@@ -26,92 +26,81 @@ console.log(equipos);
 console.log("ultimo", equipos.pop());
 
 
-/* Definir un objeto  */
-let materias = ["web","conta","costos"];
-
+/* Definir un objeto */
+let materias = ["Web","conta","costos"]
 let grupo52 = {
-     "nombre" : "grupo 52",
+     "nombre" : "grupo52",
      "semestre" : 5,
      "carrera" : "LTI",
-     "materias" : materias,
+     "materias" : materias
 };
 
 console.log(grupo52);
 
-console.log("nombre:", grupo52[0]) /* error */
-console.log("nombre:", grupo52["nombre"]) /* sí :D */
+console.log("nombre:", grupo52[0]); /* error */
+console.log("nombre:", grupo52["nombre"]);
 console.log("nombre:", grupo52.nombre);
 console.log("Primer materia:",
-             grupo52["materias"][0] );
+            grupo52["materias"][0]);
 
 console.log("Primer materia:",
-             grupo52.materias[0] );
+            grupo52.materias[0]);
 
 grupo52.facultad = "facpya";
 
 console.log(grupo52);
 
-// let carrera = prompt("¿Cuál es tu carrera?")
+/* let carrera = prompt("Cual es tu carrera"); */
 
-
-document.write("<marquee>Hola</marquee>");
+document.write("<marquee>Hola</marquee>")
 
 document.write(`<ul>
                     <li>LTI</li>
                     <li>LA</li>
                     <li>CP</li>
                     <li>LNI</li>
-                </ul>`);
+                </ul>`)
 
-// Ejemplo if
+/* ejemplo if */
 
-let calif = 90;
+let calif = 900;
 
-
-if (calif >= 95 && calif <=100) {
-    console.log("Excelente :D");
+if (calif >= 95  && calif <=100) {
+    console.log("Excelente");
 }
 else {
-    console.log("No excelente D:")
+    console.log("No excelente");
 }
 
-// sentencia condicional múltiple switch
-
-let numero = 2
+let numero = 3;
 
 switch (numero) {
     case 1:
-        console.log("uno")
+        console.log("uno");
         break;
     case 2:
-        console.log("dos")
+        console.log("dos");
         break;
     case 3:
-        console.log("tres")
+        console.log("tres");
         break;
     case 4:
-        console.log("cuatro")
+        console.log("cuatro");
         break;
     default:
-        console.log("error")
+        console.log("error");
         break;
 }
 
-// Ciclo for
-
+/* Ciclo for */
 for(let i=1; i<=100; i++){
-   let codigo = "<marquee>" + i + "</marquee>"
-   document.write(codigo);
+    let codigo = "<marquee>" + i + "</marquee>";
+    document.write(codigo);
 }
 
-for(let i=10;   ; i--){
+for(let i=10;i>=1; i--){
     document.write(i + "<br>");
 }
-
-
-/* Escribir un programa en js que permita calcular el total a pagar de un empleado. Solicitar el
-numero de horas trabajadas y el pago por hora. Pagar las horas extras al triple. Se
-consideran las primeras 40 horas como normales. */
 
 let horas = prompt("¿Cuantas horas trabajó ésta semana?");
 let sueldo = prompt("¿Cuanto gana por hora?");
@@ -128,23 +117,3 @@ else {
     let codigo1 = "<p>" + "Su sueldo de esta semana es: " + total + "</p>";
     document.write(codigo1);
 }
-
-// Escribir un programa que imprima los horarios en que debe tomar los medicamentos un paciente. Solicitar
-// al usuario la hora de la toma inicial (0-23) y la frecuencia de la toma diaria (1-12)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
