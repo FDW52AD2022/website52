898de39269b98d0732095e21b07fec03ad4f3000
gabriela jazmin orona elizondo <gabriela.oronaelzn@uanl.edu.mx>
2022-10-27 23:01:39 -0500

    subi los primeros js

--- gabrielaoelz/js/js1.js
+++ gabrielaoelz/js/js1.js
@@ -1,26 +1,21 @@
 
 console.log("Hola javascript");
 console.warn("hola a todos");
-console.error("Saludos");
+console.error("saludos") /* objeto, después del objeto es un método*/
+/* Console  clase 
+CONSOLE  constante 
+_HHH
+___JJJ___ */
 
-console.log(`El tipo de dato de false, es ${typeof false}`);
-console.log(`El tipo de dato de 5.5, es ${typeof 5.5}`);
-console.log(`El tipo de dato de js, es ${typeof 'js'}`);
+console.log(`El tipo de dato de false ${typeof false}`);
 
-console.log(`El resultado de 3>1 o 3=2 es:
-${3 > 1 || 3 == 2}`)
+console.log(`El tipo de dato de 5.5 ${typeof 5.5}`);
 
+console.log(`El tipo de dato de js ${typeof 'js'}`);
 
-console.log("La siguiente línea marca si 3 es mayor a 2, con si o no")
-console.log( 3 > 2 ? "simon, true":"nel, false" )
+console.log(`El resultado de:
+${3>1||3==2}`);
 
-console.log("La siguiente línea marca si 3 es menor a 2, con si o no")
-console.log( !3 > 2 ? "simon, true":"no, false" )
+console.log(3>2 ? "verdadero" : "falso");
 
-
-console.log("Variable numérica");
-let edad = 20;
-
-
-console.log("Constante");
-const uni="UANL";
+const UNI = "UANL";
