0ba5c64e30cae1f8a7aacb6cf911cafc73cf8588
victor gl <victor.garzaly@uanl.edu.mx>
2022-10-27 22:35:36 -0500

    modifique el ciclo if y nombres del select

--- victorlti/jscalcu.js
+++ victorlti/jscalcu.js
@@ -1,23 +1,10 @@
-let suma = parseInt(document.getElementById("select").value);
-let resta=parseInt(document.getElementById("select").value);
-let multi=parseInt(document.getElementById("select").value);
-let divi=parseInt(document.getElementById("select").value);
-
 function calcu(){
 let num1=parseInt(document.getElementById("num1").value);
 let num2=parseInt(document.getElementById("num2").value);
+let op=parseInt(document.getElementById("select").value);
 let resul=document.getElementById("resul");
 
-if(select==1){
+if(op==1){
     resul.value=num1+num2;
 }
-    else if(select==2){
-        resul.value=num1-num2;
-    }
-        else if(select==3){
-            resul.value=num1*num2;
-        }
-            else if(select==4){
-                resul.value=num1/num2;
-            }
 }
