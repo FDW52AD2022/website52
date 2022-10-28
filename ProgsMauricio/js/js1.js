5102ab5a20ce53a6c504c4f708a8ffcc183b610f
ProgsMauricio <mauricio.garciasldn@uanl.edu.mx>
2022-10-28 16:28:55 -0500

    js1yjs2

--- ProgsMauricio/js/js1.js
+++ ProgsMauricio/js/js1.js
@@ -1,27 +1,14 @@
+console.log("Hola javascript");
+console.warn("Hola a todos")
+console.error("saludos")
+console.log(`El tipo de dato 
+                de false ${typeof false}`);
+console.log(`El tipo de dato 
+                de 5.5 ${typeof false}`);
+console.log(`El tipo de dato
+                de js ${typeof 'js'}`);
+console.log(`El resultado es:
+${!3 > 1 || 3== 2}`);
+console.log(!3 > 2 ? "verdadero" : "falso");
+const UNI = "UANL";
 
-console.log ("Hola Javascript");
-console.warn ("Hola a todos");
-console.error ("Saludos");
-console.log (`El tipo de dato de false ${typeof false}`);
-
-
-console.log (`El tipo de dato de 5.5 ${typeof 5.5}`);
-
-
-console.log (`El tipo de dato de js ${typeof 'js'}`);
-
-
-console.log(`El resultado de:
-${!3 > 1 || 3 == 2}`);
-
-
-console.log( !3 > 2 ? "verdadero" :"falso");
-
-let no_tocar = 12
-
-
-no_tocar =222;
-
-const UNI="UANL";
-
-UNI = "patito";
